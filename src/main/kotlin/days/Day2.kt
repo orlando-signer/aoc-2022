61193ad68455d20afbe194e0cfdf36878141d7c1
Orlando Signer <orlando.signer@gmail.com>
2022-12-03 16:45:31 +0100

    day2 solved with different lovely matrices

--- src/main/kotlin/days/Day2.kt
+++ src/main/kotlin/days/Day2.kt
@@ -4,22 +4,31 @@ import java.lang.IllegalArgumentException
 
 class Day2 : Day(2) {
 
-    private val magicLookupMap: Map<String, Int> = mapOf(
-        "A" to 7,
-        "B" to 4,
-        "C" to 1,
-        "X" to 5,
-        "Y" to 0,
-        "Z" to 4
+    private val pointsMatrix = arrayOf(
+        arrayOf(4, 8, 3),
+        arrayOf(1, 5, 9),
+        arrayOf(7, 2, 6)
+    )
+
+    private val pointsMatrix2 = arrayOf(
+        arrayOf(3, 4, 8),
+        arrayOf(1, 5, 9),
+        arrayOf(2, 6, 7)
     )
 
     override fun partOne(): Any {
-        return inputList.map { it.split(" ") }
-            .sumOf { (magicLookupMap.getValue(it[0]) + magicLookupMap.getValue(it[1])) % 9 +1}
+        return inputList.map { inputToIndices(it) }
+            .sumOf { (a, b) -> pointsMatrix[a][b] }
     }
 
     override fun partTwo(): Any {
-        return ""
+        return inputList.map { inputToIndices(it) }
+            .sumOf { (a, b) -> pointsMatrix2[a][b] }
     }
 
+    private fun inputToIndices(input: String): Pair<Int, Int> {
+        val splitted = input.split(" ")
+        // Subtracts the Charcode of A (65) resp X (88) to get a zero-based index
+        return  Pair((splitted[0][0] - 65).code, (splitted[1][0] - 88).code)
+    }
 }
