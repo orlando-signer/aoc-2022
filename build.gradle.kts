ed1a05ea4f12d918358c395550ab35288ec64b26
Orlando Signer <orlando.signer@gmail.com>
2024-12-01 11:52:32 +0100

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     application
-    kotlin("jvm") version "1.7.21"
-    kotlin("plugin.serialization") version "1.7.21"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 application {
@@ -19,17 +17,13 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.reflections", "reflections", "0.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    implementation("org.slf4j:slf4j-nop:2.0.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.slf4j:slf4j-nop:2.0.16")
 
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.9.1")
-    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.9.1")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.5.4")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.11.3")
+    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.11.3")
+    testImplementation("io.kotest", "kotest-runner-junit5", "5.9.1")
     testImplementation("com.github.stefanbirkner:system-lambda:1.2.1")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.withType<Test> {
