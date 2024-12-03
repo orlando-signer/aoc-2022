9dcd58967430373151baf8fa9fe9cb0cb1a5a33c
Orlando Signer <orlando.signer@gmail.com>
2024-12-03 08:17:25 +0100

    2024.3.2 solved

--- src/main/kotlin/aoc/_2024/Day3.kt
+++ src/main/kotlin/aoc/_2024/Day3.kt
@@ -15,6 +15,22 @@ class Day3 : Day(3) {
     }
 
     override fun partTwo(): Any {
-        return -1
+        val mulMatches = Regex("mul\\(\\d{1,3},\\d{1,3}\\)").findAll(inputString)
+        val doMatches = Regex("do\\(\\)").findAll(inputString)
+        val dontMatches = Regex("don't\\(\\)").findAll(inputString)
+
+        val enabledAt = doMatches.map { it.range.first }
+        val disabledAt = dontMatches.map { it.range.first }
+
+        return mulMatches.filter { mulMatch ->
+            (enabledAt.findLast { it < mulMatch.range.first } ?: -1) >
+                    (disabledAt.findLast { it < mulMatch.range.first } ?: -2)
+        }.sumOf { match ->
+            match.value.split(Regex("\\D+"))
+                .filter { it.isNotBlank() }
+                .map { it.toInt() }
+                .reduce { a, b -> a * b }
+
+        }
     }
 }
