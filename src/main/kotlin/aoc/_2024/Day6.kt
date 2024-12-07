c0eaa861267d5280da077224ca4315a1bbe33713
Orlando Signer <orlando.signer@gmail.com>
2024-12-07 13:13:32 +0100

    2024.6.2 solved

--- src/main/kotlin/aoc/_2024/Day6.kt
+++ src/main/kotlin/aoc/_2024/Day6.kt
@@ -5,37 +5,72 @@ import aoc.Day
 class Day6 : Day(6) {
 
     override fun partOne(): Any {
-        var position = Point(-1, -1)
+        val position = getStartingPosition()
+
+        val visitedFields = findVisitedFields(inputList, position)
+        return visitedFields.size
+    }
+
+    private fun findVisitedFields(
+        inputList: List<String>,
+        start: Point,
+    ): Set<Point> {
+        var next = start
+        val visitedFields = mutableMapOf<Point, Int>()
+
         var direction = Direction.NORTH
-        val visitedFields = mutableSetOf<Point>()
-        inputList.forEachIndexed { y, s ->
-            val x = s.indexOf('^')
-            if (x > -1) {
-                position = Point(x, y)
-            }
-        }
-        visitedFields += position
-
-        var next = position
-
+        var position = start
         while (!isOutOfBound(next)) {
             if (inputList[next.y][next.x] == '#') {
                 direction = direction.rotate()
             } else {
                 position = next
             }
-            visitedFields += position
+            visitedFields[position] = visitedFields.getOrDefault(position, 0) + 1
+            if (visitedFields[position]!! > 4) {
+                throw LazyLoopDetectionException()
+            }
             next = position.move(direction)
         }
-        return visitedFields.size
+        return visitedFields.keys
+    }
+
+    private fun getStartingPosition(): Point {
+        inputList.forEachIndexed { y, s ->
+            val x = s.indexOf('^')
+            if (x > -1) {
+                return Point(x, y)
+            }
+        }
+        throw IllegalStateException("No starting point :(")
     }
 
     private fun isOutOfBound(point: Point): Boolean {
         return point.x < 0 || point.x > (inputList.first().length - 1) || point.y < 0 || point.y > (inputList.size - 1)
     }
 
+    // 1718 to high
     override fun partTwo(): Any {
-        return -1
+        val position = getStartingPosition()
+        var loopCount = 0
+        // den ganzen Pfad des Guardian rausfinden
+        val visitedFields = findVisitedFields(inputList, position).minus(position)
+        // auf jedem Feld ein Obstacle platzieren und gucken obs explodiert
+        for (visitedField in visitedFields) {
+            val obstacledMap = inputList.mapIndexed { y, s ->
+                if (y == visitedField.y) s.replaceRange(
+                    visitedField.x,
+                    visitedField.x + 1,
+                    "#"
+                ) else s
+            }
+            try {
+                findVisitedFields(obstacledMap, position)
+            } catch (e: LazyLoopDetectionException) {
+                loopCount++
+            }
+        }
+        return loopCount
     }
 
     data class Point(val x: Int, val y: Int) {
@@ -54,4 +89,6 @@ class Day6 : Day(6) {
             return entries[(ordinal + 1) % 4]
         }
     }
+
+    class LazyLoopDetectionException : Exception("guardian is getting tired")
 }
--- src/test/kotlin/aoc/_2024/Day6Test.kt
+++ src/test/kotlin/aoc/_2024/Day6Test.kt
@@ -12,6 +12,6 @@ class Day6Test : FunSpec({
     }
 
     test("testPartTwo") {
-        day.partTwo().shouldBe(TODO("Set value from example 2"))
+        day.partTwo().shouldBe(6)
     }
 })
