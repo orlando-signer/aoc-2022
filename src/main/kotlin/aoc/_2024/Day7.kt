fcc1161ba11ad9850a58640481a1285804832b39
Orlando Signer <orlando.signer@gmail.com>
2024-12-12 20:13:50 +0100

    2024.07 use list and index-access instead of creating always new lists

--- src/main/kotlin/aoc/_2024/Day7.kt
+++ src/main/kotlin/aoc/_2024/Day7.kt
@@ -11,42 +11,40 @@ class Day7 : Day(7) {
 
     override fun partTwo(): Any {
         return evaluate(this::reducePart2)
-
     }
 
-    private fun evaluate(reduceOp: (List<Long>, Long) -> Boolean): Long {
+    private fun evaluate(reduceOp: (List<Long>, Int, Long, Long) -> Boolean): Long {
         var result = 0L
         inputList.forEach { // Berni Tribute Code, zwischendurch ists auch mit ner Loop einfacher ;)
             val expectedValue = it.split(":").first().toLong()
             val numbers = it.split(":")[1].trim().split(" ").map { it.toLong() }
-            if (reduceOp.invoke(numbers, expectedValue)) {
+            if (reduceOp.invoke(numbers, 1, numbers.first(), expectedValue)) {
                 result += expectedValue
             }
         }
         return result
     }
 
-    private fun reduce(numbers: List<Long>, expectedValue: Long): Boolean {
-        if (numbers.size == 1) {
-            return numbers.first() == expectedValue
-        } else if (numbers.first() > expectedValue) {
+    private fun reduce(numbers: List<Long>, index: Int, currentValue: Long, expectedValue: Long): Boolean {
+        if (currentValue == expectedValue) {
+            return numbers.size == index
+        } else if (currentValue > expectedValue || index >= numbers.size ) {
             return false
         }
-        return reduce(listOf(numbers[0] * numbers[1]) + numbers.drop(2), expectedValue)
-                || reduce(listOf(numbers[0] + numbers[1]) + numbers.drop(2), expectedValue)
+        return reduce(numbers, index + 1, currentValue * numbers[index], expectedValue)
+                || reduce(numbers, index + 1, currentValue + numbers[index], expectedValue)
     }
 
-    private fun reducePart2(numbers: List<Long>, expectedValue: Long): Boolean {
-        if (numbers.size == 1) {
-            return numbers.first() == expectedValue
-        } else if (numbers.first() > expectedValue) {
+    private fun reducePart2(numbers: List<Long>, index: Int, currentValue: Long, expectedValue: Long): Boolean {
+        if (currentValue == expectedValue) {
+            return numbers.size == index
+        } else if (currentValue > expectedValue || index >= numbers.size ) {
             return false
         }
-        return reducePart2(listOf(numbers[0] * numbers[1]) + numbers.drop(2), expectedValue)
-                || reducePart2(listOf(numbers[0] + numbers[1]) + numbers.drop(2), expectedValue)
+        return reducePart2(numbers, index + 1, currentValue * numbers[index], expectedValue)
+                || reducePart2(numbers, index + 1, currentValue + numbers[index], expectedValue)
                 || reducePart2(
-            listOf((numbers[0].toString() + numbers[1].toString()).toLong()) + numbers.drop(2),
-            expectedValue
+            numbers, index + 1, (currentValue.toString() + numbers[index].toString()).toLong(), expectedValue
         )
     }
 }
